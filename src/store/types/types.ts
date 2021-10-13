7cf066c20aeb59d5dc1384db0c5e73fd45e0c339
hellmendos <poznkirill3@gmail.com>
2021-10-13 13:03:35 +0300

    Add Address type to types

--- src/store/types/types.ts
+++ src/store/types/types.ts
@@ -5,6 +5,35 @@ export interface Post {
     body: string | undefined
 }
 
+type geo = {
+    lat: string
+    lng: string
+}
+
+export interface Address {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: geo
+}
+
+export interface User {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: Address
+}
+
+export interface Post {
+    userId: number,
+    id: number,
+    title: string | undefined,
+    body: string | undefined
+}
+
+
 export interface PostInitialState {
     loading: boolean,
     errors: boolean,
